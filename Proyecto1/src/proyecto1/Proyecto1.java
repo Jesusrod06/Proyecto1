f58fe46d24ad8330c65116d0c61e67a3061f7275
Jesus <jesubrito2004@gmail.com>
2025-06-20 01:07:02 -0400

    Agrega clase FileManagerDemo.java para las pruebas de compilacion

--- Proyecto1/src/proyecto1/Proyecto1.java
+++ Proyecto1/src/proyecto1/Proyecto1.java
@@ -3,6 +3,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package proyecto1;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import Interfaces.MainFrame;
+
 
 /**
  *
@@ -15,7 +20,29 @@ public class Proyecto1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Set look and feel to system default
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | 
+                 IllegalAccessException | UnsupportedLookAndFeelException e) {
+            System.err.println("Could not set system look and feel: " + e.getMessage());
+        }
+        
+        // Start the application on the Event Dispatch Thread
+        SwingUtilities.invokeLater(() -> {
+            try {
+                MainFrame mainFrame = new MainFrame();
+                mainFrame.setVisible(true);
+                
+                System.out.println("Word Search Application started successfully!");
+                System.out.println("Load a grid file to begin searching for words.");
+                
+            } catch (Exception e) {
+                System.err.println("Error starting application: " + e.getMessage());
+                e.printStackTrace();
+            }
+        });
+    }
     }
     
-}
+
